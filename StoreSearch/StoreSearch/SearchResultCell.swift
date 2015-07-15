0cc2158112d0c5128c185fcefa9799f90490e6ca
freesaoul <anthonycamara@MacBook-Pro-de-Anthony.local>
2015-07-15 16:45:15 +0200

    Custom cell for Result

---
+++ StoreSearch/StoreSearch/SearchResultCell.swift
@@ -0,0 +1,19 @@
+//
+//  SearchResultCell.swift
+//  StoreSearch
+//
+//  Created by Anthony Camara on 15/07/2015.
+//  Copyright (c) 2015 Anthony Camara. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultCell: UITableViewCell {
+
+// MARK: - IBOutlet
+    
+    @IBOutlet weak var nameLabel:           UILabel!
+    @IBOutlet weak var artistNameLabel:     UILabel!
+    @IBOutlet weak var artworkImageView:    UIImageView!
+
+}
