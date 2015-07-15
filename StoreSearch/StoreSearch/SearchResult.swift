875e9ed5dbee1b1d80fcb76bc4171474c8a64be2
freesaoul <anthonycamara@MacBook-Pro-de-Anthony.local>
2015-07-15 15:52:08 +0200

    étape 1 - Search Bar - done

---
+++ StoreSearch/StoreSearch/SearchResult.swift
@@ -0,0 +1,21 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Anthony Camara on 15/07/2015.
+//  Copyright (c) 2015 Anthony Camara. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    
+// MARK: - Property
+    
+    var name =          ""
+    var artistName =    ""
+    
+    
+    
+    
+}
